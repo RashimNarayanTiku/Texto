672c2accff20e7848f2260bb8be35bc535a454f9
Rashim Narayan Tiku <rnt11223344@tutanota.com>
2021-01-26 22:36:07 +0530

    Minor Changes

--- texto.py
+++ texto.py
@@ -47,10 +47,6 @@ paste_icon = tk.PhotoImage(file=r'icons/paste.png')
 clear_all_icon = tk.PhotoImage(file=r'icons/clear_all.png')
 find_icon = tk.PhotoImage(file=r'icons/find_2.png')
 
-#VIEW ICONS
-tool_bar_icon = tk.PhotoImage(file=r'icons/tool_bar.png')
-status_bar_icon = tk.PhotoImage(file=r'icons/status_bar.png')
-
 #FEATURES ICONS
 spell_check_on_icon = tk.PhotoImage(file='icons/book_non_trans.png')
 spell_check_off_icon = tk.PhotoImage(file='icons/book_trans.png')
@@ -120,7 +116,7 @@ main_application.bind("<Button-3>", do_popup)
 
 # /////////////////////////////////////////////// TOOLBAR /////////////////////////////////////////////// 
 
-tool_bar = ttk.Label(main_application)
+toolbar = ttk.Label(main_application)
 
 
 ## COLOR THEME
@@ -135,7 +131,7 @@ def theme_change():
     theme_btn.config(image=color_icons[theme])
     main_application.set_theme(color_tuple[3])
 
-theme_btn = tk.Menubutton(tool_bar,image=color_icons['Light Theme'])
+theme_btn = tk.Menubutton(toolbar,image=color_icons['Light Theme'])
 theme_btn_menu = tk.Menu(theme_btn,tearoff=False)
 theme_btn.config(menu=theme_btn_menu)
 for i in color_dict:
@@ -147,14 +143,14 @@ createToolTip(theme_btn,text="Theme")
 ##font box
 font_tuples = tk.font.families()
 font_selected = tk.StringVar()
-font_box = ttk.Combobox(tool_bar,width=30,textvariable=font_selected,state='readonly',cursor='hand2')
+font_box = ttk.Combobox(toolbar,width=30,textvariable=font_selected,state='readonly',cursor='hand2')
 font_box['values'] = font_tuples
 font_box.current(font_tuples.index('Arial'))
 font_box.grid(row=0,column=1,padx=2)
 
 ##size box
 size_var = tk.IntVar()
-font_size = ttk.Combobox(tool_bar, width=14,textvariable=size_var,state='readonly',cursor='hand2')
+font_size = ttk.Combobox(toolbar, width=14,textvariable=size_var,state='readonly',cursor='hand2')
 font_size['values'] = tuple(range(8,81,2))
 font_size.current(2)
 font_size.grid(row=0,column=4,padx=2)
@@ -165,113 +161,113 @@ off_undo_icon = tk.PhotoImage(file=r'icons/off_undo.png')
 on_redo_icon = tk.PhotoImage(file=r'icons/on_redo.png')
 off_redo_icon = tk.PhotoImage(file=r'icons/off_redo.png')
 
-undo_btn = ttk.Label(tool_bar,image=off_undo_icon,cursor='hand2')
+undo_btn = ttk.Label(toolbar,image=off_undo_icon,cursor='hand2')
 undo_btn.grid(row=0,column=5,padx=1)
 createToolTip(undo_btn,"Undo")
-redo_btn = ttk.Label(tool_bar,image=off_redo_icon,cursor='hand2')
+redo_btn = ttk.Label(toolbar,image=off_redo_icon,cursor='hand2')
 redo_btn.grid(row=0,column=6,padx=1)
 createToolTip(redo_btn,"Redo")
 
-separator = ttk.Separator(tool_bar, orient=tk.VERTICAL).grid(row=0,column=7,padx=6,sticky="sn")
+separator = ttk.Separator(toolbar, orient=tk.VERTICAL).grid(row=0,column=7,padx=6,sticky="sn")
 
 ##font size changer buttons
 text_size_up_icon = tk.PhotoImage(file='icons/resize-font-up.png')
 text_size_down_icon = tk.PhotoImage(file='icons/resize-font-down.png')
 
-text_size_up_btn = ttk.Label(tool_bar,image=text_size_up_icon,cursor='hand2')
+text_size_up_btn = ttk.Label(toolbar,image=text_size_up_icon,cursor='hand2')
 text_size_up_btn.grid(row=0,column=9,padx=2)
 createToolTip(text_size_up_btn,"Bigger Text")
-text_size_down_btn = ttk.Label(tool_bar,image=text_size_down_icon,cursor='hand2')
+text_size_down_btn = ttk.Label(toolbar,image=text_size_down_icon,cursor='hand2')
 text_size_down_btn.grid(row=0,column=10,padx=2)
 createToolTip(text_size_down_btn,"Smaller Text")
 
 ## font color button
 font_color_icon = tk.PhotoImage(file='icons/font_color.png')
-font_color_btn = ttk.Label(tool_bar,image=font_color_icon,cursor='hand2')
+font_color_btn = ttk.Label(toolbar,image=font_color_icon,cursor='hand2')
 font_color_btn.grid(row=0,column=11,padx=2)
 createToolTip(font_color_btn,"Color Palette")
 
-separator = ttk.Separator(tool_bar, orient=tk.VERTICAL).grid(row=0,column=12,padx=6,sticky="sn")
+separator = ttk.Separator(toolbar, orient=tk.VERTICAL).grid(row=0,column=12,padx=6,sticky="sn")
 
 ##bold button
 off_bold_icon = tk.PhotoImage(file='icons/off_bold.png')
 on_bold_icon = tk.PhotoImage(file='icons/on_bold.png')
-bold_btn = ttk.Label(tool_bar,image=off_bold_icon,cursor='hand2')
+bold_btn = ttk.Label(toolbar,image=off_bold_icon,cursor='hand2')
 bold_btn.grid(row=0,column=13,padx=2)
 createToolTip(bold_btn,"Bold")
 
 ##italic button
 off_italic_icon = tk.PhotoImage(file='icons/off_italic.png')
 on_italic_icon = tk.PhotoImage(file='icons/on_italic.png')
-italic_btn = ttk.Label(tool_bar,image=off_italic_icon,cursor='hand2')
+italic_btn = ttk.Label(toolbar,image=off_italic_icon,cursor='hand2')
 italic_btn.grid(row=0,column=14,padx=2)
 createToolTip(italic_btn,"Italics")
 
 ##underline button
 off_underline_icon = tk.PhotoImage(file='icons/off_underline.png')
 on_underline_icon = tk.PhotoImage(file='icons/on_underline.png')
-underline_btn = ttk.Label(tool_bar,image=off_underline_icon,cursor='hand2')
+underline_btn = ttk.Label(toolbar,image=off_underline_icon,cursor='hand2')
 underline_btn.grid(row=0,column=15,padx=2)
 createToolTip(underline_btn,"Underline")
 
-separator = ttk.Separator(tool_bar, orient=tk.VERTICAL).grid(row=0,column=16,padx=6,sticky="sn")
+separator = ttk.Separator(toolbar, orient=tk.VERTICAL).grid(row=0,column=16,padx=6,sticky="sn")
 
 ##align left
 on_align_left_icon = tk.PhotoImage(file='icons/align-left-2x.png')
-align_left_btn = ttk.Label(tool_bar,image = on_align_left_icon,cursor='hand2')
+align_left_btn = ttk.Label(toolbar,image = on_align_left_icon,cursor='hand2')
 align_left_btn.grid(row=0,column=17,padx=2)
 createToolTip(align_left_btn,"Align Left")
 
 ##align center
 on_align_center_icon = tk.PhotoImage(file='icons/align-center-2x.png')
-align_center_btn = ttk.Label(tool_bar,image=on_align_center_icon,cursor='hand2')
+align_center_btn = ttk.Label(toolbar,image=on_align_center_icon,cursor='hand2')
 align_center_btn.grid(row=0,column=18,padx=5)
 createToolTip(align_center_btn,"Align Center")
 
 ##align right
 on_align_right_icon = tk.PhotoImage(file='icons/align-right-2x.png')
-align_right_btn = ttk.Label(tool_bar,image = on_align_right_icon,cursor='hand2')
+align_right_btn = ttk.Label(toolbar,image = on_align_right_icon,cursor='hand2')
 align_right_btn.grid(row=0,column=19,padx=2)
 createToolTip(align_right_btn,"Align Right")
 
-separator = ttk.Separator(tool_bar, orient=tk.VERTICAL).grid(row=0,column=20,padx=6,sticky="sn")
+separator = ttk.Separator(toolbar, orient=tk.VERTICAL).grid(row=0,column=20,padx=6,sticky="sn")
 
 ## text to speech
-tts_btn = ttk.Label(tool_bar,image=tts_on_icon,cursor='hand2')
+tts_btn = ttk.Label(toolbar,image=tts_on_icon,cursor='hand2')
 tts_btn.grid(row=0,column=21, padx=2)
 createToolTip(tts_btn,"Text To Speech")
 
 
 ## Spell check icon
 dictionary = enchant.Dict("en_US")
-spell_check_btn = ttk.Label(tool_bar,image=spell_check_on_icon,cursor='hand2')
+spell_check_btn = ttk.Label(toolbar,image=spell_check_on_icon,cursor='hand2')
 spell_check_btn.grid(row=0,column=22,padx=2)
 createToolTip(spell_check_btn,"Spell checker")
 
-separator = ttk.Separator(tool_bar, orient=tk.VERTICAL).grid(row=0,column=23,padx=6,sticky="sn")
+separator = ttk.Separator(toolbar, orient=tk.VERTICAL).grid(row=0,column=23,padx=6,sticky="sn")
 
 ## find button
-find_btn = ttk.Label(tool_bar,image=find_icon,cursor='hand2')
+find_btn = ttk.Label(toolbar,image=find_icon,cursor='hand2')
 find_btn.grid(row=0, column=24, padx=2)
 createToolTip(find_btn, "Find and Replace")
 
 
 ## find and replace labels and entry fields
 find_state = 0
-find_label = ttk.Label(tool_bar,text='Find',font=('InkFree',9))
+find_label = ttk.Label(toolbar,text='Find',font=('InkFree',9))
 find_label.grid(row=0,column=25)
 find_var = tk.StringVar()
-find_entry = ttk.Entry(tool_bar,textvariable=find_var,width=20)
+find_entry = ttk.Entry(toolbar,textvariable=find_var,width=20)
 find_entry.grid(row=0,column=26)
 
-replace_label=ttk.Label(tool_bar,text='Replace',font=('InkFree',9))
+replace_label=ttk.Label(toolbar,text='Replace',font=('InkFree',9))
 replace_label.grid(row=0,column=27)
 replace_var = tk.StringVar()
-replace_entry = ttk.Entry(tool_bar,textvariable=replace_var,width=20)
+replace_entry = ttk.Entry(toolbar,textvariable=replace_var,width=20)
 replace_entry.grid(row=0,column=28)
 
 submit_icon = tk.PhotoImage(file='icons/enter.png')
-submit_btn = ttk.Label(tool_bar,image = submit_icon)
+submit_btn = ttk.Label(toolbar,image = submit_icon)
 submit_btn.grid(row=0,column=29)
 
 find_label.grid_remove()
@@ -556,8 +552,8 @@ text_area.bind("<space>",spell_check)
 
 # ///////////////////////////////////////////////  STATUS BAR ///////////////////////////////////////////////
 
-status_bar = ttk.Label(main_application)
-status_bar.grid_columnconfigure(3, weight=1)
+statusbar = ttk.Label(main_application)
+statusbar.grid_columnconfigure(3, weight=1)
 
 
 def changed(event=None):
@@ -585,14 +581,14 @@ text_area.bind('<<Modified>>',changed)
 
 
 ##Line and Column counter
-lineNumberLabel = ttk.Label(status_bar,width=10, text=f"Ln: {1}  Col: {0}")
+lineNumberLabel = ttk.Label(statusbar,width=10, text=f"Ln: {1}  Col: {0}")
 lineNumberLabel.config(font=("Calibri",8))
 lineNumberLabel.grid(row=0,column=0,padx=4)
 
-separator = ttk.Separator(status_bar, orient=tk.VERTICAL).grid(row=0,column=1,padx=6,sticky="sn")
+separator = ttk.Separator(statusbar, orient=tk.VERTICAL).grid(row=0,column=1,padx=6,sticky="sn")
 
 ##word and character counter
-countLabel = ttk.Label(status_bar, width=30, text=f"0 words")
+countLabel = ttk.Label(statusbar, width=30, text=f"0 words")
 countLabel.config(font=("Calibri",8))
 countLabel.grid(row=0,column=2)
 
@@ -615,7 +611,7 @@ main_application.attributes('-fullscreen', False)
 main_application.bind("<F11>", toggle_full_screen)
 main_application.bind("<Escape>", quit_full_screen)
 
-fullscreen_btn = ttk.Label(status_bar,image=fullscreen_icon,cursor='hand2')
+fullscreen_btn = ttk.Label(statusbar,image=fullscreen_icon,cursor='hand2')
 fullscreen_btn.bind('<ButtonRelease-1>',toggle_full_screen)
 
 fullscreen_btn.grid(row=0,column=30,sticky='e')
@@ -737,35 +733,35 @@ Edit.add_command(label='Clear All',image=clear_all_icon,compound=tk.LEFT,command
 
 
 # VIEW MENU FUNCTIONALITY
-tool_bar_state = tk.BooleanVar()
-tool_bar_state.set(True)
+toolbar_state = tk.BooleanVar()
+toolbar_state.set(True)
 status_bar_state = tk.BooleanVar()
 status_bar_state.set(True)
 line_bar_state = tk.BooleanVar()
 line_bar_state.set(True)
 
 def show_hide_toolbar():
-    global tool_bar_state,status_bar_state
-    if tool_bar_state:
-        tool_bar.pack_forget()
-        tool_bar_state=False
+    global toolbar_state,status_bar_state
+    if toolbar_state:
+        toolbar.pack_forget()
+        toolbar_state=False
     else:
         text_editor.pack_forget()
-        status_bar.pack_forget()
-        tool_bar.pack(side=tk.TOP,fill=tk.X)
+        statusbar.pack_forget()
+        toolbar.pack(side=tk.TOP,fill=tk.X)
         text_editor.pack(fill=tk.BOTH,expand=1)
 
         if status_bar_state:
-            status_bar.pack(side=tk.BOTTOM, fill=tk.X)
-        tool_bar_state=True
+            statusbar.pack(side=tk.BOTTOM, fill=tk.X)
+        toolbar_state=True
         
 def show_hide_statusbar():
     global status_bar_state
     if status_bar_state:
-        status_bar.pack_forget()
+        statusbar.pack_forget()
         status_bar_state=False
     else:
-        status_bar.pack(side=tk.BOTTOM, fill=tk.X)
+        statusbar.pack(side=tk.BOTTOM, fill=tk.X)
         status_bar_state=True
 
 def show_hide_linebar():
@@ -780,7 +776,7 @@ def show_hide_linebar():
         text_editor.text.pack(side=tk.LEFT,fill=tk.BOTH, expand=True)
         text_editor.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         line_bar_state=True
-View.add_checkbutton(label='Tool Bar',compound=tk.LEFT,variable=tool_bar_state, onvalue=True, offvalue=False, command=show_hide_toolbar)
+View.add_checkbutton(label='Tool Bar',compound=tk.LEFT,variable=toolbar_state, onvalue=True, offvalue=False, command=show_hide_toolbar)
 View.add_checkbutton(label='Status Bar',compound=tk.LEFT, variable=status_bar_state, onvalue=True, offvalue=False,command=show_hide_statusbar)
 View.add_checkbutton(label='Line Number Bar',compound=tk.LEFT, variable=line_bar_state, onvalue=True, offvalue=False,command=show_hide_linebar)
 
@@ -852,8 +848,8 @@ text_area.bind_all("<Control-t>",tts)
 
 # ///////////////////////////////////////////////  APPLICATION  /////////////////////////////////////////////// 
  
-tool_bar.pack(side=tk.TOP,fill=tk.X)
-status_bar.pack(side=tk.BOTTOM,fill=tk.X)
+toolbar.pack(side=tk.TOP,fill=tk.X)
+statusbar.pack(side=tk.BOTTOM,fill=tk.X)
 text_editor.pack(fill=tk.BOTH,expand=1)
 text_area.focus_set()
 
